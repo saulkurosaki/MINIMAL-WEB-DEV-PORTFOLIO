eb36d46fc8eda1774dd021986c3dca4a73a3999d
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-07 13:19:15 -0600

    feat(Hero): implement the complete hero section animated titles and subtitles

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -29,8 +29,12 @@ const Hero = () => {
 
           <TextGenerateEffect
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
-            words="Transforming Concepts into Seamless Experiences"
+            words="Transforming Concepts into Seamless User Experiences"
           />
+
+          <p className="text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi!, I&apos;m Saul, a Next.js developer based in Mexico.
+          </p>
         </div>
       </div>
     </div>
