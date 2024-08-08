a19a07b0d7552bbfe6e220d844d737ccc5d16019
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-07 22:57:00 -0600

    feat(data/index.ts): implement the complete socialMedia constant data array

--- data/index.ts
+++ data/index.ts
@@ -201,17 +201,17 @@ export const workExperience = [
   },
 ];
 
-// export const socialMedia = [
-//   {
-//     id: 1,
-//     img: "/git.svg",
-//   },
-//   {
-//     id: 2,
-//     img: "/twit.svg",
-//   },
-//   {
-//     id: 3,
-//     img: "/link.svg",
-//   },
-// ];
+export const socialMedia = [
+  {
+    id: 1,
+    img: "/git.svg",
+  },
+  {
+    id: 2,
+    img: "/twit.svg",
+  },
+  {
+    id: 3,
+    img: "/link.svg",
+  },
+];
