88953470476416489ee68f0510c74af4bd8b4523
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-08 17:40:58 -0600

    feat(Clients): implement the InfiniteMovingCards component along as its principal props

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,10 +1,22 @@
+import { testimonials } from "@/data";
+import { InfiniteMovingCards } from "./ui/InfiniteMovingCards";
+
 const Clients = () => {
   return (
     <div className="py-20" id="projects">
       <h1 className="heading">
         Kind words from <span className="text-purple">satisfied clients</span>
       </h1>
-      <div className="flex flex-col items-center"></div>
+      <div className="flex flex-col items-center">
+        <div className="h-[50vh] md:h-[30rem] rounded-md flex flex-col antialiased items-center relative overflow-hidden">
+          <InfiniteMovingCards
+            items={testimonials}
+            direction="right"
+            speed="slow"
+            pauseOnHover={true}
+          />
+        </div>
+      </div>
     </div>
   );
 };
