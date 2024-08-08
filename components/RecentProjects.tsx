32c07abea8dda8de5558f3e7de88749393175261
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-08 11:43:59 -0600

    feat(RecentProjects): implement the title rendering along the project card layout for each personal project

--- components/RecentProjects.tsx
+++ components/RecentProjects.tsx
@@ -1,3 +1,5 @@
+import { projects } from "@/data";
+
 const RecentProjects = () => {
   return (
     <div className="py-20">
@@ -5,6 +7,17 @@ const RecentProjects = () => {
         A small selection of{" "}
         <span className="text-purple">recent projects</span>
       </h1>
+
+      <div className="flex flex-wrap items-center justify-center p-4 gap-16 mt-10">
+        {projects.map(({ id, title, des, img, iconLists, link }) => (
+          <div
+            className="lg:min-h-[32.5rem] h-[25rem] flex items-center justify-center sm:w-96 w-[80vw]"
+            key={id}
+          >
+            {title}
+          </div>
+        ))}
+      </div>
     </div>
   );
 };
