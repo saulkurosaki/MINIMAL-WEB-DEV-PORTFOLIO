618c2fdebf4f13063eac377672f3c101bcfc5410
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-08 17:29:49 -0600

    feat(Clients): implement the clients section title

--- components/Clients.tsx
+++ components/Clients.tsx
@@ -1,5 +1,12 @@
 const Clients = () => {
-  return <div>Clients</div>;
+  return (
+    <div className="py-20" id="projects">
+      <h1 className="heading">
+        Kind words from <span className="text-purple">satisfied clients</span>
+      </h1>
+      <div className="flex flex-col items-center"></div>
+    </div>
+  );
 };
 
 export default Clients;
