fc7541347b2fd7f7cd944265dde58dca593ef29a
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-08 21:24:06 -0600

    fix the section routing layout bug

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -5,7 +5,7 @@ import MagicButton from "./MagicButton";
 
 const Footer = () => {
   return (
-    <footer className="w-full pt-20 pb-10" id="contact">
+    <footer className="w-full pt-20 pb-20" id="contact">
       {/* background grid */}
       <div className="w-full absolute left-0 -bottom-72 min-h-96">
         <img
@@ -24,7 +24,7 @@ const Footer = () => {
           Reach out to me today and let&apos;s discuss how I can help you
           achieve your goals.
         </p>
-        <a href="mailto:contact@jsmastery.pro">
+        <a href="mailto:saulkurosaki13@gmail.com">
           <MagicButton
             title="Let's get in touch"
             icon={<FaLocationArrow />}
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -5,7 +5,7 @@ import { TextGenerateEffect } from "./ui/TextGenerateEffect";
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
+    <div className="pb-20 pt-32">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
