13c4908731cae62fe15fae8b32fc5731326c178f
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-31 23:55:04 -0600

    feat(Home): implement the BlobCursor component within the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -10,10 +10,12 @@ import Approach from "@/components/Approach";
 import Experience from "@/components/Experience";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
+import BlobCursor from "@/components/BlobCursor";
 
 const Home = () => {
   return (
     <main className="relative bg-black-100 flex justify-center items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <BlobCursor />
       <div className="max-w-7xl w-full">
         <FloatingNav navItems={navItems} />
         <Hero />
