18090bca6529a5723945fd71ab40a75feadb75c8
saulkurosaki <saulkurosaki13@gmail.com>
2024-09-01 17:09:54 -0600

    feat(Footer): implement the BlobCursor behind the social media buttons feature

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -44,6 +44,7 @@ const Footer = () => {
               key={info.id}
               target="_blank"
               rel="noopener noreferrer"
+              className="relative z-[60]"
             >
               <div className="w-10 h-10 cursor-pointer flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180 bg-opacity-75 bg-black-200 rounded-lg border border-black-300">
                 <img src={info.img} alt="icons" width={20} height={20} />
