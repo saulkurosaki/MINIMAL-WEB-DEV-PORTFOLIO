93957f9243e9ebd814004138dc6d8cca9caad9b9
saulkurosaki <saulkurosaki13@gmail.com>
2024-08-31 21:54:22 -0600

    feat(Experience): implement the first work experience section card info

--- data/index.ts
+++ data/index.ts
@@ -232,8 +232,8 @@ export const companies = [
 export const workExperience = [
   {
     id: 1,
-    title: "Frontend Engineer Intern",
-    desc: "Assisted in the development of a web-based platform using React.js, enhancing interactivity.",
+    title: "Meta Frontend Student",
+    desc: "Developed and maintained web applications ensuring cross-browser compatibility with cross-functional teams creating efficient, scalable and high-quality products.",
     className: "md:col-span-2",
     thumbnail: "/exp1.svg",
   },
