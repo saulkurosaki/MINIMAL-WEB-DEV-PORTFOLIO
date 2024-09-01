2d7c89443e5846f70384d6b3e1ec68fc40eea3b7
saulkurosaki <saulkurosaki13@gmail.com>
2024-09-01 17:04:54 -0600

    feat(Approach): implement the BlobCursor behind the cards feature

--- components/Approach.tsx
+++ components/Approach.tsx
@@ -88,7 +88,7 @@ const Card = ({
       onMouseLeave={() => setHovered(false)}
       // change h-[30rem] to h-[35rem], add rounded-3xl
       className="border border-black/[0.2] group/canvas-card flex items-center justify-center
-       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative lg:h-[35rem] rounded-3xl "
+       dark:border-white/[0.2]  max-w-sm w-full mx-auto p-4 relative z-[60] lg:h-[35rem] rounded-3xl "
       style={{
         //   add these two
         //   you can generate the color from here https://cssgradient.io/
